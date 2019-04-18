de3501a1d6d88fea50846a45d05e2367a5f6a26f
yoyoacg <1031060898@qq.com>
2019-04-18 15:12:57 +0800

    canps

--- application/api/controller/Tiku.php
+++ application/api/controller/Tiku.php
@@ -132,6 +132,34 @@ class Tiku extends Api
         $this->result('success', $result, 200);
     }
 
+    /**
+     * 获取模拟试题
+     * @throws \think\Exception
+     */
+    public function mn(){
+        $type = $this->request->post('type','公务员');
+        $page = $this->request->param('page', 1);
+        $pageSize = $this->request->param('page_size', 10);
+        if (intval($page) < 1) $page = 1;
+        $offset = ($page - 1) * intval($pageSize);
+        $category = ItemCategory::where('type',$type)->column('id');
+        $where = [
+            'cid' => ['IN',$category]
+        ];
+        $total = ItemBank::where($where)->count('id');
+        $list = ItemBank::where($where)
+            ->group('rand()')
+            ->limit($offset,$pageSize)
+            ->column('id,type,content,val,a,b,c,d,analysis');
+        $result = [
+            'total' => $total,
+            'currentPage' => $page,
+            'list' => array_values($list)
+        ];
+        $this->result('success', $result, 200);
+    }
+
+
 
 
 }
