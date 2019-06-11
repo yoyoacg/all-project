55adf9e52be346f25218781bb58633bd4e230d40
yoyoacg <1031060898@qq.com>
2019-06-11 11:07:41 +0800

    sport

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -22,28 +22,9 @@ class Index extends Api
         $this->success('请求成功');
     }
     public function getip(){
-//        $ip = $this->request->ip();
-        $ip = $this->get_real_ip();
+        $ip = $this->request->ip();
         $data=['ip'=>$ip];
         $this->result('',$data);
-    }
-
-    protected function get_real_ip(){
-        $ip=false;
-        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-            $ip=$_SERVER['HTTP_CLIENT_IP'];
-        }
-        if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-            $ips=explode (', ', $_SERVER['HTTP_X_FORWARDED_FOR']);
-            if($ip){ array_unshift($ips, $ip); $ip=FALSE; }
-            for ($i=0; $i < count($ips); $i++){
-                if(!eregi ('^(10│172.16│192.168).', $ips[$i])){
-                    $ip=$ips[$i];
-                    break;
-                }
-            }
-        }
-        return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
     }
 
 }
