ed5bf0252611b97539c3bb2a5b25bcdcd5f5029e
yoyoacg <1031060898@qq.com>
2019-06-17 16:31:33 +0800

    sport

---
+++ application/common/model/KeyStorage.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class KeyStorage extends Model
+{
+    /**
+     * 自定义键值存储表
+     * @var string
+     */
+    protected $table='key_storage';
+
+}
